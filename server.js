6baa15b4b09539fb7425c09ee2ee3c9a3f42f6f0
Bruno Cerqueira <bcerqueira94@gmail.com>
2017-11-26 23:59:08 -0200

    Install MongoDB.

--- server.js
+++ server.js
@@ -1,16 +1,24 @@
 const express = require('express');
+const bodyParser= require('body-parser')
 const app = express();
+const MongoClient = require('mongodb').MongoClient
 
-app.listen(3000, function() {
-    console.log('listening on 3000')
-  })
+var db
 
-  app.get('/', (req, res) => {
-    res.sendFile(__dirname + '/public/index.html')
-    // Note: __dirname is directory that contains the JavaScript source code. Try logging it and see what you get!
-    // Mine was '/Users/zellwk/Projects/demo-repos/crud-express-mongo' for this app.
-  })
+app.use(bodyParser.urlencoded({extended: true}))
 
-  app.post('/quotes', (req, res) => {
-    console.log('Hellooooooooooooooooo!')
-  })
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/public/index.html')
+})
+
+app.post('/quotes', (req, res) => {
+    console.log(req.body)
+})
+
+MongoClient.connect('mongodb://<dbuser>:<dbpassword>@ds121456.mlab.com:21456/si6-crud-js', (err, database) => {
+    if (err) return console.log(err)
+        db = database
+    app.listen(3000, () => {
+      console.log('listening on 3000')
+    })
+})
