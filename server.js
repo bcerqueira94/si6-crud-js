9ec3694a744612238bf7af33f98dfbf3eb5061f2
Bruno Cerqueira <bcerqueira94@gmail.com>
2017-11-27 01:11:50 -0200

    Implement DELETE from MongoDB.

--- server.js
+++ server.js
@@ -5,10 +5,10 @@ const MongoClient = require('mongodb').MongoClient
 
 var db
 
-
-app.set('view engine', 'ejs')
-
 app.use(bodyParser.urlencoded({extended: true}))
+app.set('view engine', 'ejs')
+app.use(express.static('public'))
+app.use(bodyParser.json())
 
 app.get('/', (req, res) => {
     db.collection('quotes').find().toArray((err, result) => {
@@ -34,3 +34,27 @@ MongoClient.connect('mongodb://bcerqueira94:crkgpgvt25@ds121456.mlab.com:21456/s
       console.log('listening on 3000')
     })
 })
+
+app.delete('/quotes', (req, res) => {
+    db.collection('quotes').findOneAndDelete({name: req.body.name},
+    (err, result) => {
+      if (err) return res.send(500, err)
+      res.send({message: 'A darth vadar quote got deleted'})
+    })
+  })
+
+  app.put('/quotes', (req, res) => {
+    db.collection('quotes')
+    .findOneAndUpdate({name: 'Yoda'}, {
+      $set: {
+        name: req.body.name,
+        quote: req.body.quote
+      }
+    }, {
+      sort: {_id: -1},
+      upsert: true
+    }, (err, result) => {
+      if (err) return res.send(err)
+      res.send(result)
+    })
+  })
